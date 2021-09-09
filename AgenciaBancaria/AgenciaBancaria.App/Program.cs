108840456873a86b4d8f804fed0443403ea9e12d
Rodrigo Maurício <85081181+RodrigoMauricio7@users.noreply.github.com>
2021-09-09 11:29:04 -0300

    Add files via upload

---
+++ AgenciaBancaria/AgenciaBancaria.App/Program.cs
@@ -0,0 +1,38 @@
+﻿using AgenciaBancaria.Dominio;
+using System;
+
+namespace AgenciaBancaria.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Endereco endereco = new Endereco("Rua Antonio fagundes",
+                "11443-000", "Guarujá", "São Paulo");
+
+                Cliente cliente = new Cliente ("Rodrigo", "123456789", "9745612", endereco);
+
+                ContaCorrente conta = new ContaCorrente(cliente, 100);
+
+                string senha = "abcd1234";
+
+                conta.Abrir(senha);
+
+                Console.WriteLine("Conta: " + conta.Situacao + ": " conta.NumeroConta + "-" conta.DigitoVerificador);
+
+                conta.Sacar(10,senha);
+
+                Console.WriteLine("Saldo: " + conta.Saldo);
+
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+
+        }
+    }
+}
---
+++ AgenciaBancaria/AgenciaBancaria.Dominio/ValidacoesBase.cs
@@ -0,0 +1,12 @@
+﻿namespace AgenciaBancaria.Dominio
+{
+    public static class ValidacoesBase
+    {
+        public string validaStringVazia(string texto)
+        {
+            return string.IsNullOrWhiteSpace(texto) ?
+                throw new Exception("A propriedade deve está preenchida")
+                : texto;
+        }
+    }
+}
